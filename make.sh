3871a3d757a8a416e239ed7b9219a40371c1229f
Federico Poli <federico.poli@cern.ch>
2014-07-04 10:26:45 +0200

    Corrects configuration step

--- make.sh
+++ make.sh
@@ -1,39 +1,57 @@
 #!/bin/bash -x
 
-INVENIO_IMAGE="fedux/invenio"
-MYSQL_IMAGE="mysql"
-REDIS_IMAGE="redis"
+BASE_PATH=$(pwd)
+
+INVENIO_BASE_IMAGE="fedux/invenio-web-base"
+MYSQL_BASE_IMAGE="mysql"
+REDIS_BASE_IMAGE="redis"
+
+INVENIO_IMAGE="fedux/invenio-web"
+MYSQL_IMAGE="fedux/invenio-mysql"
+REDIS_IMAGE="fedux/invenio-redis"
+
+SUFFIX="$RANDOM"
+
+INVENIO_CONTAINER="invenio-web-$SUFFIX"
+MYSQL_CONTAINER="invenio-mysql-$SUFFIX"
+REDIS_CONTAINER="invenio-redis-$SUFFIX"
 
 MYSQL_ROOT_PASSWORD="$RANDOM"
 
 case "$1" in
 	build)
-		docker build -t $INVENIO_IMAGE base
-		docker pull $MYSQL_IMAGE
-		docker pull $REDIS_IMAGE
+		docker build -t $INVENIO_BASE_IMAGE base
+		docker pull $MYSQL_BASE_IMAGE
+		docker pull $REDIS_BASE_IMAGE
 		;;
 
 	configure)
-		MYSQL_CONTAINER=$( docker run -d -e MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD" $MYSQL_IMAGE )
-		REDIS_CONTAINER=$( docker run -d $REDIS_IMAGE )
-		INVENIO_CONTAINER=$( docker run \
+		docker run -d \
+			--name $MYSQL_CONTAINER \
+			-e MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD" \
+			$MYSQL_BASE_IMAGE
+		docker run -d \
+			--name $REDIS_CONTAINER \
+			$REDIS_BASE_IMAGE
+		docker run \
+			--name $INVENIO_CONTAINER \
 			--link $MYSQL_CONTAINER:mysql \
 			--link $REDIS_CONTAINER:redis \
-			--volume configure:/home/docker/configure \
-			$INVENIO_IMAGE \
-			"cd configure && ./configure.sh"
-		)
+			--volume $BASE_PATH/configure:/home/docker/configure \
+			$INVENIO_BASE_IMAGE \
+			./configure/configure.sh
 
 		exit 1
 
 		docker stop $MYSQL_CONTAINER $REDIS_CONTAINER
 
+		docker commit $INVENIO_CONTAINER $INVENIO_IMAGE
 		docker commit $MYSQL_CONTAINER $MYSQL_IMAGE
 		docker commit $REDIS_CONTAINER $REDIS_IMAGE
 		;;
 
 	unit-tests)
-		docker run --rm $INVENIO_IMAGE:latest /opt/invenio/bin/inveniocfg --run-unit-tests
+		docker run --rm $INVENIO_IMAGE /opt/invenio/bin/inveniocfg --run-unit-tests
 		;;
 
 	install-demo)
@@ -59,7 +77,7 @@ case "$1" in
 			--link $MYSQL_CONTAINER:mysql \
 			--link $REDIS_CONTAINER:redis \
 			$INVENIO_IMAGE \
-			"serve -b 0.0.0.0 &; /opt/invenio/bin/inveniocfg --run-regression-tests")
+			/bin/bash -c "serve -b 0.0.0.0 &; /opt/invenio/bin/inveniocfg --run-regression-tests")
 
 		docker stop $MYSQL_CONTAINER $REDIS_CONTAINER
 		;;
