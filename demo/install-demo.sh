2c1cdd4843a9c2b76103603ec8d1fbd02eeb8187
Federico Poli <federico.poli@cern.ch>
2014-07-04 15:52:16 +0200

    Waits for MySQL

--- demo/install-demo.sh
+++ demo/install-demo.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+RET=1
+while [[ RET -ne 0 ]]; do
+    echo "Waiting for MySQL..."
+    sleep 2
+    mysql -h "$MYSQL_PORT_3306_TCP_ADDR" \
+          -u root -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" \
+          -e "status" > /dev/null 2>&1
+    RET=$?
+done
+
 /opt/invenio/bin/inveniocfg --create-demo-site
 /opt/invenio/bin/inveniocfg --load-demo-records
